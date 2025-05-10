572cb6d78f198751bcb4692e7e5ecc27a1cf072e
Guilherme <jiklgam3@gmail.com>
2025-05-10 15:49:11 -0300

    feat: integrar json-server para transações e adicionar contexto para gerenciamento de estado
    - Adicionado json-server como uma dependência de desenvolvimento e criado um arquivo server.json para simular dados de transações.
    - Introduzido TransactionsContext para gerenciar o estado da transação e fornecê-lo à aplicação.
    - Atualizado o componente App para encapsular Transactions no TransactionProvider.
    - Refatorado o componente Summary para exibir dados dinâmicos de transações usando um hook useSummary personalizado.
    - Modificado o componente Transactions para buscar e exibir transações do contexto.
    - Aprimorado o NewTransactionModal para usar o RadioGroup da Radix UI para seleção do tipo de transação.
    - Atualizados os estilos dos botões de tipo de transação para refletir os estados marcado e desmarcado.
    - Adicionadas funções utilitárias para formatação de datas e preços.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,40 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryContainer, SymmaryCard } from "./styles";
+import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-    return (
-      <SummaryContainer>
-        <SymmaryCard>
-          <header>
-            <span>Entradas</span>
-            <ArrowCircleUp size={32} color="#00b37e" />
-          </header>
 
-          <strong>R$ 17.400,00</strong>
-        </SymmaryCard>
+  const summary = useSummary()
 
-        <SymmaryCard>
-          <header>
-            <span>Saidas</span>
-            <ArrowCircleDown size={32} color="#f75a68" />
-          </header>
+  return (
+    <SummaryContainer>
+      <SymmaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
 
-          <strong>R$ 17.400,00</strong>
-        </SymmaryCard>
+        <strong>{ priceFormatter.format(summary.income) }</strong>
+      </SymmaryCard>
 
-        <SymmaryCard variant="green">
-          <header>
-            <span>Total</span>
-            <CurrencyDollar size={32} color="#fff" />
-          </header>
+      <SymmaryCard>
+        <header>
+          <span>Saidas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
 
-          <strong>R$ 17.400,00</strong>
-        </SymmaryCard>
-      </SummaryContainer>
-    );
+        <strong>{ priceFormatter.format(summary.outcome) }</strong>
+      </SymmaryCard>
+
+      <SymmaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>{ priceFormatter.format(summary.total) }</strong>
+      </SymmaryCard>
+    </SummaryContainer>
+  );
 }
